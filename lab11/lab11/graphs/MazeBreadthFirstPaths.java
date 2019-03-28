ef0fabab7cd5fafbfcc411359a3f8f0df6b16d9c
Xiaomeng Li <674409326@qq.com>
2019-03-28 15:48:13 +0800

    commit code in lab11Spring2018, cycle check task has some bugs (don't know how to deal with edgeTo[] array, without which I cannot track the relation between parent nodes and children nodes)

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,6 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
 /**
  *  @author Josh Hug
  */
@@ -9,21 +10,55 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Queue<Integer> pq;
+    private boolean targetFound;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        pq = new Queue<> ();
+        pq.enqueue(s);
+        marked[0] = true;
+        targetFound = false;
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+
+        if (pq.isEmpty()) {
+            return;
+        }else {
+            int cur = pq.dequeue();
+            if (cur == t) targetFound = true;
+            if (targetFound) return;
+
+            for (int w : maze.adj(cur)) {
+                if (!marked[w]) {
+                    pq.enqueue(w);
+                    marked[w] = true;
+                    edgeTo[w] = cur;
+                    distTo[w] = distTo[cur] + 1;
+                    announce();
+                    if (targetFound) return;
+                }
+            }
+            bfs();
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,4 +1,7 @@
 package lab11.graphs;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.Stack;
 
 /**
  *  @author Josh Hug
@@ -9,16 +12,55 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private boolean[] onStack;
+    private Stack cycle;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m; //我加的，是否需要呢？
+        onStack = new boolean[maze.N() * maze.N()];
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        int s = 0;
+        distTo[s] = 0;
+        bfs(s);
     }
 
     // Helper methods go here
+    private void bfs(int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (this.hasCycle()) {return;}
+            else if (!marked[w]) {
+                edgeTo[w] = v;
+                distTo[w] = distTo[v] + 1;
+                bfs(w);
+            }
+            else if (marked[w] && edgeTo[v] != w) {
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(v);
+                cycle.push(w);
+                edgeTo = new int[maze.N() * maze.N()];
+                int start = (Integer) cycle.pop();
+                for (int i = 0; i < cycle.size() - 1; i ++) {
+                    edgeTo[start] = (Integer) cycle.pop();
+                    announce();
+                    start = edgeTo[start];
+                }
+                return;
+            }
+        }
+    }
+
+    private boolean hasCycle() {return cycle != null;}
 }
 
